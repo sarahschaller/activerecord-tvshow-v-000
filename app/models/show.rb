6f7f6c6044c90e84c4e33edfe095c9515407c51f
Sarah Schaller <sarcsch@aol.com>
2017-09-20 13:16:56 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,8 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.find_by(rating: self.highest_rating)
+    show = self.highest_rating 
+    Show.find_by(rating: show)
   end
 
   def self.lowest_rating
